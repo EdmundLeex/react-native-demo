8a7a9ef6334f08f8fa33b9f0bbd2068228141d4e
Edmund Li <edmund.xz.lee@gmail.com>
2016-03-08 09:00:29 -0800

    react-native app

---
+++ index.ios.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+import { SearchPage } from './SearchPage';
+
+class PropertyFinderApp extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinderApp);
